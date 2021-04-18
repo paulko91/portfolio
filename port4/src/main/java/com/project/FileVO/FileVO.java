41dbfcff192d56a91b2a6ebc5aac560bbc20b2f0
paulko91 <72064433+paulko91@users.noreply.github.com>
2021-04-19 01:34:42 +0900

    파일 리스트 게시판(다운로드, 업로드) 2021.02.09

---
+++ port4/src/main/java/com/project/FileVO/FileVO.java
@@ -0,0 +1,24 @@
+package com.project.FileVO;
+
+public class FileVO {
+
+	private String fileName;	// 파일 이름
+	private String fileSize;	// 파일 사이즈
+
+	
+	
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(String fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	
+}
